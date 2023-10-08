8b0fbfad9e4e4b4c1d9371ebca7603ca75409b32
EliasLG <eliaslopez55@hotmail.com>
2023-10-08 16:32:05 -0700

    Primer commit. Incluye: - Detección de imágenes. - Rotación y escala de modelos.

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+
+public class Rotation : MonoBehaviour
+{
+    private float sensitivity = 0.15f;
+    private Vector3 mouseReference;
+    private Vector3 mouseOffset;
+    private Vector3 rotation = Vector3.zero;
+    private bool isRotating;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRotating)
+        {
+            //Offset
+            mouseOffset = (Input.mousePosition - mouseReference);
+
+            //Apply rotation
+            rotation.y = -(mouseOffset.x + mouseOffset.y) * sensitivity;
+
+            //Rotate
+            gameObject.transform.Rotate(rotation);
+
+            //Store new mouse position
+            mouseReference = Input.mousePosition;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        // rotating flag
+        isRotating = true;
+
+        // store mouse position
+        mouseReference = Input.mousePosition;
+    }
+
+    void OnMouseUp()
+    {
+        // rotating flag
+        isRotating = false;
+    }
+}
